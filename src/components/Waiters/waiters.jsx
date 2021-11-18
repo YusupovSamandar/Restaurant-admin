63dfcca6d7c5505174234c50be0283486e25a59e
samandar <yusupovvsamandar@gmail.com>
2021-11-18 16:03:02 +0500

    add login pass

--- src/components/Waiters/waiters.jsx
+++ src/components/Waiters/waiters.jsx
@@ -10,6 +10,8 @@ export default function Waiters() {
     const [columns] = useState([
         { title: 'Name', field: 'name', validate: rowData => rowData.name === '' ? { isValid: false, helperText: 'Name cannot be empty' } : true, },
         { title: 'Surname', field: 'surname', validate: rowData => rowData.surname === '' ? { isValid: false, helperText: 'Surname cannot be empty' } : true, },
+        { title: 'Login', field: 'loginName', validate: rowData => rowData.loginName === '' ? { isValid: false, helperText: 'Login Name cannot be empty' } : true, },
+        { title: 'Password', field: 'loginPassword', validate: rowData => rowData.loginPassword === '' ? { isValid: false, helperText: 'Login Password missing' } : true, },
         { title: "Phone-Number", field: 'phoneNumber' }
     ]);
     const dispatch = useDispatch();
@@ -44,10 +46,9 @@ export default function Waiters() {
                 onRowUpdate: (newData, oldData) =>
                     new Promise((resolve, reject) => {
                         setTimeout(() => {
-                            axios.put(`http://localhost:4000/data/waiters/${oldData.name}`, { name: newData.name, surname: newData.surname, phoneNumber: newData.phoneNumber }).then(() => {
+                            axios.put(`http://localhost:4000/data/waiters/${oldData.name}`, { name: newData.name, surname: newData.surname, phoneNumber: newData.phoneNumber, loginName: newData.loginName, loginPassword: newData.loginPassword }).then(() => {
                                 updateGlobalWaiters();
                             });
-
                             resolve();
                         }, 1000)
                     }),
